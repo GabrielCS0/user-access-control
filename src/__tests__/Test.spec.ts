43821841c8ccc91a2b78002ea42fbd27f043d396
Gabriel <gabrielcemp@gmail.com>
2021-03-12 12:02:47 -0300

    ESLint & Jest

---
+++ src/__tests__/Test.spec.ts
@@ -0,0 +1,4 @@
+test('it should be ok', () => {
+  const x = "1"
+  expect(x).toEqual("1")
+})
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.json({ message: "Hello, World!" })
+  return res.json({ message: 'Hello, World!' })
 })
 
 app.listen(5000, () => console.log('Server is Runing!'))
